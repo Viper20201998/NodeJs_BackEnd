dcc4e95cc0f651493cdd7a15589c87b27f271285
Viper20201998 <lesterasmethsandoval@gmail.com>
2023-01-15 19:01:42 -0600

    Modulo 5 clase 1 y 2

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override');
 const app = express();
 
 const taskRoutes = require('./routes/tasks_routes');
+const registrationsRoutes = require('./routes/registration_routes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
@@ -14,5 +15,6 @@ app.use(methodOverride('_method'));
 app.set('view engine', 'pug');
 
 app.use(taskRoutes);
+app.use(registrationsRoutes);
 
 app.listen(3000);
