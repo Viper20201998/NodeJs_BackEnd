e4609c3e373cbae19a14beb0ce14f0ef6eaf8a63
Viper20201998 <lesterasmethsandoval@gmail.com>
2023-01-21 13:06:01 -0600

    Modulo 5 clase 8 Inicio de sesion

--- models/user.js
+++ models/user.js
@@ -28,6 +28,30 @@ module.exports = (sequelize, DataTypes) => {
 			modelName: 'User',
 		}
 	);
+	User.login = function (email, password) {
+		return User.findOne({
+			where: {
+				email: email,
+			},
+		}).then((user) => {
+			if (!user) return null;
+			return user
+				.autenticatePassword(password)
+				.then((valid) => (valid ? user : null));
+		});
+	};
+
+	User.prototype.autenticatePassword = function (password) {
+		return new Promise((res, rej) => {
+			//retornamos una promesa
+			bcrypt.compare(password, this.password_hash, function (err, valid) {
+				//bcrypt.compare recibe como parametro 1-password en texto plano, 2-password_hash que esta guadado en la base de datos, 3- una funcion callback
+				if (err) return rej(err);
+				res(valid);
+			});
+		});
+	};
+
 	User.beforeCreate(function (user, options) {
 		return new Promise((res, rej) => {
 			if (user.password) {
