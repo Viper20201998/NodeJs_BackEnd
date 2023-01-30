fb86ceb254bf6b99149cad605e07cade010e327f
Viper20201998 <lesterasmethsandoval@gmail.com>
2023-01-30 12:19:47 -0600

    Modulo 5 clase 10

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -5,7 +5,12 @@ module.exports = {
 	},
 	create: function (req, res) {
 		User.login(req.body.email, req.body.password)
-			.then((user) => res.json(user))
+			.then((user) => {
+				if (user) {
+					req.session.useId = user.id;
+				}
+				res.json(user);
+			})
 			.catch((err) => {
 				console.log(err);
 				res.json(err);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 const methodOverride = require('method-override');
+const session = require('express-session');
 
 const app = express();
 
@@ -12,8 +13,15 @@ const sessionsRoutes = require('./routes/sessions_routes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
 app.set('view engine', 'pug');
+
+app.use(
+	session({
+		secret: ['adfkafajdfaldfhahfafh', 'akdfakfadfkalfsflsfafa'],
+		saveUninitialized: false,
+		resave: false,
+	})
+);
 
 app.use(taskRoutes);
 app.use(registrationsRoutes);
